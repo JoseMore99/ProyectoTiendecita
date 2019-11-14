7996328fc937170963f96aaa10e9159b9acf3479
JoseMore99 <57239404+JoseMore99@users.noreply.github.com>
2019-11-14 17:56:28 -0600

    intentando avanzar :c

---
+++ [IPC1]Proyecto2/src/ipc1/proyecto2/Facturas.java
@@ -0,0 +1,17 @@
+
+package ipc1.proyecto2;
+
+/**
+ *
+ * @author Moreira Paz
+ */
+public class Facturas {
+    
+    public int numero,nit, descuento,subtotal, total;
+    public String Tipo, nombre_emisor,tipo_cliente, fecha, direccion, numero_tarjeta;
+    
+    public Facturas(){
+        
+    }
+    
+}
